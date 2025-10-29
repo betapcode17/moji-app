d1032af42a6fe70322d08b26ece4bbed59f3ce4c
betapcode17 <quocdat19991712@gmail.com>
2025-10-29 22:25:38 +0700

    done backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
-
+import userRoute from "./routes/userRouter.js";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
 dotenv.config();
 const app = express();
 
@@ -16,7 +17,10 @@ app.use(cookieParser());
 
 //public routes
 app.use("/api/auth", authRoute);
+
 //private routes
+app.use(protectedRoute);
+app.use("api/users", userRoute);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
